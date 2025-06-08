c8eeb9aa62d9ff9f63ac8d991012ccbf0a81fb4d
Moulay Idriss Semlali <bookingflyers@gmail.com>
2025-06-08 20:40:05 +0200

    Correction du fichier start.sh pour Render

--- start.sh
+++ start.sh
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
-uvicorn app.main:app --host 0.0.0.0 --port 10000
-=======
 #!/bin/bash
+
 uvicorn app.main:app --host 0.0.0.0 --port 10000
->>>>>>> 629d0183c6c852b71ba1f9192f4ef4cd414fa5ad
+
